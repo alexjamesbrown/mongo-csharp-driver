17294e92987a7d893714ea3bccefd38d2c8a9a4a
Alex Brown <alex@alexjamesbrown.com>
2014-08-11 22:31:54 +0100

    added MongoDBClassMap
    When overridden, Map takes an instance of BsonClassMap<T>
    
    Allows BsonClassMap to be composed in separate classes

---
+++ Bson/Serialization/Serializers/MongoDBClassMap.cs
@@ -0,0 +1,36 @@
+﻿/* Copyright 2010-2012 10gen Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+namespace MongoDB.Bson.Serialization.Serializers
+{
+    /// <summary>
+    /// Base class for class map that can be registered as separate classes.
+    /// </summary>
+    public abstract class MongoDBClassMap<T>
+    {
+        protected MongoDBClassMap()
+        {
+            if (!BsonClassMap.IsClassMapRegistered(typeof(T)))
+                BsonClassMap.RegisterClassMap<T>(Map);
+        }
+
+        /// <summary>
+        /// Map method that is overridden in implementations
+        /// Takes an instance of BsonClassMap of same type as the MongoDBClassMap
+        /// </summary>
+        /// <param name="bsonClassMap"></param>
+        public abstract void Map(BsonClassMap<T> bsonClassMap);
+    }
+}
---
+++ BsonUnitTests/DefaultSerializer/MongoDBClassMapTests.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using NUnit.Framework;
+
+namespace MongoDB.BsonUnitTests.DefaultSerializer
+{
+    [TestFixture]
+    public class MongoDBClassMapTests
+    {
+        private static bool __testAlreadyRan;
+
+        public class MyClass
+        {
+            public ObjectId Id;
+            public int X;
+        }
+
+        public class MyClassClassMap : MongoDBClassMap<MyClass>
+        {
+            public override void Map(BsonClassMap<MyClass> cm)
+            {
+                cm.AutoMap();
+            }
+        }
+
+        [Test]
+        public void BsonClassMapIsClassMapRegisteredWhenMongoDBClassMapIsCreated()
+        {
+            // test can only be run once
+            if (__testAlreadyRan)
+            {
+                return;
+            }
+            else
+            {
+                __testAlreadyRan = true;
+            }
+
+            Assert.IsFalse(BsonClassMap.IsClassMapRegistered(typeof(MyClass)));
+
+            //create an instance of the ClassMap
+            new MyClassClassMap();
+
+            Assert.IsTrue(BsonClassMap.IsClassMapRegistered(typeof(MyClass)));
+        }
+    }
+}
