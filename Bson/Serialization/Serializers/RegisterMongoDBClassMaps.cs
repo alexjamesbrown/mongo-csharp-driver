de469a4e1d0f147578d3efa05139bf036af10f64
Alex Brown <alex@alexjamesbrown.com>
2014-08-11 22:52:27 +0100

    Added a static class with method to initialise (via reflection) all classes that inherit from MongoDBClassMap

---
+++ Bson/Serialization/Serializers/RegisterMongoDBClassMaps.cs
@@ -0,0 +1,45 @@
+﻿/* Copyright 2010-2012 10gen Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace MongoDB.Bson.Serialization.Serializers
+{
+    /// <summary>
+    /// Register MongoDBClassMaps
+    /// </summary>
+    public class RegisterMongoDBClassMaps
+    {
+        /// <summary>
+        /// Will create instances of classes that inherit MongoDBClassMap in the specified assemblies
+        /// </summary>
+        /// <param name="assemblies"></param>
+        public static void ForAssemblies(params Assembly[] assemblies)
+        {
+            foreach (var assembly in assemblies)
+            {
+                var classMaps = assembly
+                    .GetTypes()
+                    .Where(t => t.BaseType != null && t.BaseType.IsGenericType &&
+                                t.BaseType.GetGenericTypeDefinition() == typeof(MongoDBClassMap<>));
+
+                foreach (var classMap in classMaps)
+                    Activator.CreateInstance(classMap);
+            }
+        }
+    }
+}
---
+++ BsonUnitTests/DefaultSerializer/RegisterMongoDBClassMapTests.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using NUnit.Framework;
+
+namespace MongoDB.BsonUnitTests.DefaultSerializer
+{
+    [TestFixture]
+    public class RegisterMongoDBClassMapTests
+    {
+        private static bool __testAlreadyRan;
+
+        public class MyClass2
+        {
+            public ObjectId Id;
+            public int X;
+        }
+
+        public class MyClass2ClassMap : MongoDBClassMap<MyClass2>
+        {
+            public override void Map(BsonClassMap<MyClass2> cm)
+            {
+                cm.AutoMap();
+            }
+        }
+
+        [Test]
+        public void RegistersClassMapsInAssembly()
+        {
+            // test can only be run once
+            if (__testAlreadyRan)
+            {
+                return;
+            }
+            else
+            {
+                __testAlreadyRan = true;
+            }
+
+            Assert.IsFalse(BsonClassMap.IsClassMapRegistered(typeof(MyClass2)));
+
+            //for this assembly
+            RegisterMongoDBClassMaps.ForAssemblies(GetType().Assembly);
+
+            Assert.IsTrue(BsonClassMap.IsClassMapRegistered(typeof(MyClass2)));
+        }
+    }
+}
